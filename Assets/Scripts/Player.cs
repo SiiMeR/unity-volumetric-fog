dcc76af60fa50efbd83cfc291cb9275651cf5aea
Siim Raudsepp <siimraudsepp@msn.com>
2019-12-02 22:09:50 +0200

    Make Player script look a bit nicer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,47 +12,40 @@ public class Player : MonoBehaviour
 	[SerializeField] private float _YSensitivity = 1;
 
 	private bool _isCursorLocked;
-	
+	private Camera _camera;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		_camera = Camera.main;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-		
-		float yRot = Input.GetAxisRaw("Mouse X") * _XSensitivity;
-		float xRot = Input.GetAxisRaw("Mouse Y") * _YSensitivity;
+	void Update (){
+		var yRot = Input.GetAxisRaw("Mouse X") * _XSensitivity;
+		var xRot = Input.GetAxisRaw("Mouse Y") * _YSensitivity;
 		
 		var ms = _MoveSpeed;
-		
 		if (Input.GetKey(KeyCode.LeftShift))
 		{
 			ms *= 2;
 		}
-		
-		transform.position += Camera.main.transform.forward * ms * Input.GetAxis("Vertical") * Time.deltaTime;
-		transform.position += Camera.main.transform.right * ms * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+		var position = transform.position;
+		position += Time.deltaTime * ms * Input.GetAxis("Vertical") * _camera.transform.forward;
+		position += Time.deltaTime * ms * Input.GetAxis("Horizontal") * _camera.transform.right;
+		transform.position = position;
 
 		transform.localRotation *= Quaternion.Euler (0f, yRot, 0f);
 		
-		var cameras = gameObject.GetComponentsInChildren<Camera>();
+		_camera.transform.localRotation *= Quaternion.Euler(-xRot, 0f, 0f);
 		
-		foreach (var cam in cameras)
-		{
-			cam.transform.localRotation *= Quaternion.Euler(-xRot, 0f, 0f);
-		}
-		
-	
 		UpdateCursorLock();
 	}
 	
 	//
 	// CODE BELOW IS FROM UNITY STANDARD ASSETS MouseLook.cs
 	//
-
-	
 	public void UpdateCursorLock()
 	{
 		//if the user set "lockCursor" we check & properly lock the cursos
