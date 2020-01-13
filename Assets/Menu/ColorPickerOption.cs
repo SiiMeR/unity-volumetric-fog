02f7f00d764657f152237316dc658bd249faf665
Siim Raudsepp <siimraudsepp@msn.com>
2020-01-14 00:00:05 +0200

    Menu: Change color picker to a UI based one

---
+++ Assets/Menu/ColorPickerOption.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPickerOption : MonoBehaviour
+{
+    [SerializeField] private ColorPicker _colorPicker;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _colorPicker.onValueChanged.AddListener(color =>
+        {
+            print(color);
+        });       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
